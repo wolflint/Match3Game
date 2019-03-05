b9d446ea2fc3ac6472d78b837031fe711d1d06a1
Marcin Wolf <wolflint@github.com>
2019-03-05 23:23:08 +0000

    Bug fixes

--- scripts/Goal.gd
+++ scripts/Goal.gd
@@ -5,6 +5,7 @@ export (Texture) var goal_texture
 export (int) var max_needed
 export (String) var goal_string
 var number_collected = 0
+var goal_met = false
 
 func check_goal(goal_type):
 	if goal_type == goal_string:
@@ -14,4 +15,5 @@ func update_goal(goal_type):
 	if number_collected < max_needed:
 		number_collected += 1
 	if number_collected == max_needed:
-		print(goal_type + " goal met")
+		if !goal_met:
+			goal_met = true
