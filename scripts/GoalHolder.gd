941cccd2b47e515843005d04005430d4b6afb5da
wolflint <wolflint@github.com>
2019-03-13 17:12:44 +0000

    File renames and file hierarchy fixes

--- scripts/GoalHolder.gd
+++ scripts/GoalHolder.gd
@@ -13,9 +13,20 @@ func create_goals():
 func check_goals(goal_type):
 	for i in get_child_count():
 		get_child(i).check_goal(goal_type)
+	check_game_win()
+
+func goals_met():
+	for i in get_child_count():
+		if !get_child(i).goal_met:
+			return false
+		return true
+
+func check_game_win():
+	if goals_met():
+		print('You won!')
 
 func _on_Grid_check_goal(goal_type):
 	check_goals(goal_type)
 
-func _on_IceHolder_break_ice():
-	pass # replace with function body
+func _on_IceHolder_break_ice(goal_type):
+	check_goals(goal_type)
