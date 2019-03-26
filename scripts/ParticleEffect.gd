7aae0a982a7932d0bbf588da48f3007d9a45be27
wolflint <wolflint@github.com>
2019-03-26 09:30:35 +0000

    Fixed light green particles

--- scripts/ParticleEffect.gd
+++ scripts/ParticleEffect.gd
@@ -6,7 +6,7 @@ func change_color(effect_color):
 		"yellow": $Particles2D.modulate = Color("#ffa700")
 		"green": $Particles2D.modulate = Color("#008744")
 		"orange": $Particles2D.modulate = Color("#FFA500")
-		"lgreen": $Particles2D.modulate = Color("#00FF00")
+		"lightGreen": $Particles2D.modulate = Color("#77dd77")
 		"pink": $Particles2D.modulate = Color("#FF00FF")
 		"red": $Particles2D.modulate = Color("#FF0000")
 		"purple": $Particles2D.modulate = Color("#800080")
