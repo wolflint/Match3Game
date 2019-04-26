ad4569224fad08664e32ae5e62fc653a24dc7d91
Marcin Wolf <mrcnwolf@gmail.com>
2019-04-26 07:21:15 +0100

    Battery indicator in game window

--- scripts/BatteryIndicator.gd
+++ scripts/BatteryIndicator.gd
@@ -1,3 +1,4 @@
+tool
 extends TextureProgress
 
 func _ready():
@@ -5,8 +6,10 @@ func _ready():
 	update_indicator()
 
 func update_indicator():
+	var current_value = value
 	value = GameDataManager.get_current_points()
 	$Label.text = str(GameDataManager.get_current_points())
+	$Tween.interpolate_property(self, "value", current_value, GameDataManager.get_current_points(), 0.3, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
 
 func _on_points_changed():
 	update_indicator()
